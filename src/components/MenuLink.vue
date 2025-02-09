150c2ea78b2cd12057135cd31f5b0e28f82eaf66
Michał Wieczorek <michal.wieczorek@scandiweb.com>
2025-02-09 19:32:15 +0100

    Dodaj pierwsza reklame

---
+++ src/components/MenuLink.vue
@@ -0,0 +1,85 @@
+<style scoped>
+.menu-link {
+  position: relative;
+  z-index: 2;
+
+  width: 144px;
+  height: 60px;
+  background-color: rgba(122, 159, 191, 0.8);
+  border-block: 1px solid rgb(92, 129, 161);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+}
+
+.menu-link::before,
+.menu-link::after {
+  content: '';
+  display: block;
+  position: absolute;
+  z-index: -1;
+  inset-block: -1px;
+  width: 50px;
+  border-radius: 100%;
+  background-color: rgba(122, 159, 191, 0.8);
+}
+
+.menu-link::before {
+  right: 100%;
+  transform: translateX(50%);
+  clip-path: inset(0 50% 0 0);
+  border: 1px solid rgb(92, 129, 161);
+}
+
+.menu-link::after {
+  left: 100%;
+  transform: translateX(-50%);
+  clip-path: inset(0 0 0 50%);
+  border: 1px solid rgb(92, 129, 161);
+}
+
+.menu-link span {
+  font-size: 14px;
+  line-height: 1.2;
+  color: #fff;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.menu-link__icon {
+  width: 71px;
+  height: 50px;
+  transform-origin: center;
+  transform: translateX(12px) rotate(-45deg);
+}
+
+@media (min-width: 768px) {
+  .menu-link {
+    width: 180px;
+  }
+
+  .menu-link span {
+    font-size: 18px;
+  }
+}
+</style>
+
+<template>
+  <router-link class="menu-link" to="/menu">
+    <span>Przejdź<br />do menu</span>
+    <HandTapIcon aria-hidden class="menu-link__icon" />
+  </router-link>
+</template>
+
+<script>
+import HandTapIcon from '@/assets/icons/hand-tap.svg'
+
+export default {
+  name: 'MenuLink',
+  components: {
+    HandTapIcon,
+  },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,150 @@
+<style scoped>
+main {
+  width: 100%;
+  min-height: 100dvh;
+  background: linear-gradient(to bottom, #bfcde2, #c0cdd3);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+
+.text-container {
+  flex-grow: 1;
+  margin-block-start: 16px;
+  text-align: center;
+  font-weight: 700;
+}
+
+.text-container .small-text {
+  font-size: 14px;
+  line-height: 1.33;
+}
+
+.text-container span {
+  display: block;
+  font-size: 32px;
+  line-height: 36px;
+}
+
+.text-container .highlight {
+  background: black;
+  color: white;
+  display: inline-block;
+  padding: 5px 10px;
+}
+
+.bottom-container {
+  margin-block-end: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
+
+@media (min-width: 360px) {
+  .bottom-container {
+    margin-block-end: 16px;
+  }
+}
+
+@media (min-width: 414px) {
+  .text-container {
+    margin-block-start: 30px;
+  }
+
+  .text-container .small-text {
+    font-size: 15px;
+  }
+
+  .text-container span {
+    font-size: 44px;
+    line-height: 48px;
+  }
+
+  .bottom-container {
+    margin-block-end: 24px;
+  }
+}
+
+@media (min-width: 768px) {
+  main {
+    padding: 24px;
+    align-items: center;
+    gap: 40px;
+  }
+
+  .text-container {
+    flex-grow: 0;
+    margin-block-start: 0;
+  }
+
+  .text-container span {
+    font-size: 51px;
+    line-height: 56px;
+  }
+
+  .bottom-container {
+    margin-block-end: 0;
+  }
+
+  .bottom-container img {
+    width: 400px;
+  }
+}
+
+@media (min-width: 834px) {
+  main {
+    gap: 60px;
+  }
+}
+
+@media (min-width: 1024px) {
+  main {
+    padding: 40px;
+  }
+
+  .text-container .small-text {
+    font-size: 18px;
+  }
+
+  .text-container span {
+    font-size: 60px;
+    line-height: 64px;
+  }
+
+  .bottom-container {
+    gap: 40px;
+  }
+}
+</style>
+
 <template>
   <main>
-    <h1>Home view</h1>
-    <CoffeeCup
-      :size="250"
-      :ingredients="{
-        50: {
-          text: 'espresso',
-          background: ['#402d20', '#4f3727', '#583d2b', '#634632', '#6f4d36'],
-          contrast: 'dark',
-        },
-        80: {
-          text: 'ice cream',
-          background: ['#71A6D1', '#85B5D9', '#99C4E1', '#ACD3E8', '#C0E2F0'],
-        },
-        90: {
-          text: 'whipped cream',
-          background: ['#FEF3B3', '#FEF7C0', '#FEFCD6', '#FEFEF1', '#FFFEFE'],
-        },
-        100: {
-          text: 'chocolate',
-          background: ['#3F0110', '#4B0B0D', '#5B1F07', '#6F3203', '#7B3E00'],
-          contrast: 'dark',
-        },
-      }"
-    />
+    <div class="text-container">
+      <p class="small-text">w naszych automatach</p>
+      <span>SPRÓBUJ</span>
+      <span class="highlight">PRAWDZIWEGO</span>
+      <span>AROMATU</span>
+      <span class="highlight">ŚWIEŻO</span>
+      <span>ZMIELONEGO</span>
+      <span>ZIARNA</span>
+    </div>
+    <div class="bottom-container">
+      <img src="/images/beans2.png" alt="coffee beans" width="300" />
+      <MenuLink />
+    </div>
   </main>
 </template>
 
 <script>
-import CoffeeCup from '@/components/CoffeeCup.vue'
+import MenuLink from '@/components/MenuLink.vue'
 
 export default {
-  components: { CoffeeCup },
+  name: 'HomeView',
+  components: { MenuLink },
 }
 </script>
